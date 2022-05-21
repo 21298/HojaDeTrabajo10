c3d115d412109f8fa79c0fb649feeaf21a68faa3
Bryan España <83832357+BryanEspana@users.noreply.github.com>
2022-05-20 22:06:44 -0600

    Implementación de vista

---
+++ Vista.java
@@ -0,0 +1,23 @@
+/*
+ * Universidad del Valle de Guatemala
+ * Ingeneria en Ciencias de la Computacion y TI
+ * Departamento de Computacion
+ * Hoja de Trabajo 10  Grafos
+ * 
+ * Integrantes
+ * Angel Gabriel Perez Figueroa   | 21298
+ * Bryan Carlos España Machorro   | 21550
+ * Javier Alejandro Prado Ramirez | 21486
+ * Gabriel Estuardo Garcia Donis  | 21352
+ */
+
+public class Vista {
+    
+    public void Ciudades() {
+        System.out.println("0. Mixco");
+        System.out.println("1. Antigua");
+        System.out.println("2. Escuintla");
+        System.out.println("3. Santa Lucia");
+        System.out.println("4. Guatemala\n");
+      }
+}
